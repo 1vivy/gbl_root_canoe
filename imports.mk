602b75f33d4a38d48465ebce36c9e2c2cc79171d
1vivy <1vivy@tutanota.com>
2026-09-06 02:09:50 -0600

    build: refresh GUI recovery bundle

--- imports.mk
+++ imports.mk
@@ -2,7 +2,7 @@
 # Regenerate with make bump or make import-pin ID=<id>.
 
 CANOE_WEBUI_VERSION = 0.1.0
-CANOE_WEBUI_SHA256 = ae0d34452a0fb301dce993aaca98665a88d14ccdc574d736215f2e35603e4efd
+CANOE_WEBUI_SHA256 = eb933853b3bbcd4ef9fedfee6454ddf88985f50e4f19d5a2cbc37025493bc9e9
 CANOE_WEBUI_URL = file://$(CANOE_ROOT_DIR)/targets/magisk_module/webui-cache/canoe-boot-manager-0.1.0.tar.gz
 CANOE_WEBUI_PATH = $(CANOE_ROOT_DIR)/targets/magisk_module/webui-cache/canoe-boot-manager-0.1.0.tar.gz
 
